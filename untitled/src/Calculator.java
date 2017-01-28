f957389c27eacc2f0821c1e8bbc4268a31c98b88
Sonikpalms <metlin.igor@gmail.com>
2017-01-28 20:24:44 +0200

    56

--- untitled/src/Calculator.java
+++ untitled/src/Calculator.java
@@ -4,19 +4,27 @@
 
 import java.util.Scanner;
 
+
 public class Calculator {
-    public static void main (String[] args){
+    public static void main (String[] args) {
 
-        Scanner num = new Scanner(System.in); // создаём объект класса Scanner
+        Scanner num = new Scanner(System.in); 
         System.out.print("Please input first number: ");
-        double  num_1 = num.nextDouble();
+        double num_1 = num.nextDouble();
         System.out.print("Please input second number: ");
-        double  num_2 = num.nextDouble();
-        System.out.println("Sum is: "  + (num_1 + num_2));
-        System.out.println("Difference is: "  + (num_1 - num_2));
-        System.out.println("Multiplication is: "  + (num_1 * num_2));
-        System.out.println("Division is: "  + (num_1 / num_2));
-
+        double num_2 = num.nextDouble();
+        System.out.println("Sum is: " + (num_1 + num_2));
+        System.out.println("Difference is: " + (num_1 - num_2));
+        System.out.println("Multiplication is: " + (num_1 * num_2));
+        if (num_2 == 0)
+        {
+            System.out.println("Division is: Don't delete on zero");
+        }
+        else
+        {
+            System.out.println("Division is: " + (num_1 / num_2));
+        }
+    }
 
     }
-}
+
