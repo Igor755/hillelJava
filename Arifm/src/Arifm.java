d559a00bc9a70919d63426ba78aec368f4bf8c49
Sonikpalms <metlin.igor@gmail.com>
2017-02-01 21:59:34 +0200

    les2

--- Arifm/src/Arifm.java
+++ Arifm/src/Arifm.java
@@ -1,10 +1,22 @@
 /**
  * Created by Sonikpalms on 31.01.2017.
  */
+
+import java.util.Scanner;
+
 public class Arifm {
+
     public static void main(String[] args) {
 
+        Scanner num = new Scanner(System.in);
+        System.out.print("Please input first number: ");
+        double num_1 = num.nextDouble();
+        System.out.print("Please input second number: ");
+        double num_2 = num.nextDouble();
 
-        System.out.println("kjnkk");
+        double num_3 = ((num_1 + num_2)/2);
+        //int num_4 = (int)num_3;
+
+        System.out.println("Arithmetical mean: " + num_3 );
     }
 }
