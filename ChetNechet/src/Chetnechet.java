f4c9bcbc5f3754c7624b58c03a013c7327079185
Sonikpalms <metlin.igor@gmail.com>
2017-02-01 22:48:18 +0200

    les2_2

--- ChetNechet/src/Chetnechet.java
+++ ChetNechet/src/Chetnechet.java
@@ -1,5 +1,27 @@
 /**
  * Created by Sonikpalms on 01.02.2017.
  */
+import java.util.Scanner;
+
+
 public class Chetnechet {
+
+    public static void main(String[] args)
+
+    {
+
+        Scanner num = new Scanner(System.in);
+        System.out.print("Please input first number: ");
+        int num_1 = num.nextInt();
+
+        if(num_1 % 2 == 0)
+        {
+            System.out.print("Chetnoe");
+        }
+        else
+        {
+            System.out.print("Nechetnoe");
+        }
+
+    }
 }
